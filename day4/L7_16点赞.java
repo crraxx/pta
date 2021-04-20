4a6d96dbbcdd459589269a6c74a2680ed3b19dfe
crraxx <78258888+crraxx@users.noreply.github.com>
2021-04-21 00:00:31 +0800

    Add files via upload

---
+++ day4/L7_16点赞.java
@@ -0,0 +1,14 @@
+package day4;
+
+import java.util.Scanner;
+
+public class L7_16点赞 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int[] num = new int[N];
+        for (int i = 0; i < N; i++) {
+            
+        }
+    }
+}
